27714f22e5e4c28edb9ce741088bfe5d0a54ce82
pzabrocki <pzzabrocki@gmail.com>
2014-11-07 20:43:20 +0100

    Access control to backend by enforcing an active status and minimum value to the role of the user (at least admin role). Access control to frontend by enforcing an active status of the user.

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -48,21 +48,40 @@ class LoginForm extends Model
     }
 
     /**
-     * Logs in a user using the provided username and password.
+     * Logs in an active user using the provided username and password.
      *
      * @return boolean whether the user is logged in successfully
      */
     public function login()
     {
-        if ($this->validate()) {
+        if ($this->validate() && $this->getUser()->status_id == ValueHelpers::getStatusValue('Active')) {
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         } else {
             return false;
         }
     }
+    
+    /**
+     * Control access to the admin login by requiring the user to have at least a role of admin and an active status.
+     * @return type
+     * @throws NotFoundHttpException
+     */
+    public function loginAdmin() 
+    {
+        if (($this->validate()) && $this->getUser()->role_id >= ValueHelpers::getRoleValue('Admin') && $this->getUser()->status_id == ValueHelpers::getStatusValue('Active')) {
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        } else {
+            throw new NotFoundHttpException('You Shall Not Pass.');
+        }       
+
+    }       
 
     /**
      * Finds user by [[username]]
+     * Since we know the private attribute $_user defaults to false, 
+     * the condition in the if statement is going to be met if this method has not already been run. 
+     * So if there is no username in $_user, then it uses a static method of the User model 
+     * to return a model instance of the user and set it to $_user.
      *
      * @return User|null
      */
