127d89865df79c66aa4ca247cfddcf87b0f80f95
pzabrocki <pzzabrocki@gmail.com>
2014-11-12 12:44:22 +0100

    We created an upgrade controller and view and then enforced rules to bring the user to the page if they didn’t meet the minimum user type allowed for access. We did this by adding a PermissionsHelper method, requireUpgradeTo(‘Paid’) to the update method on the profile controller. We also added a requirement for the user to have a status of active. This tightens up security and access and did this by adding a matchCallback requirement to the access rules in the behaviors method.

---
+++ frontend/views/upgrade/index.php
@@ -0,0 +1,8 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h3>Hey "<?php echo $name->first_name; ?>", this Requires Upgrade</h3>
+
+<p>
+You can get the access you want by upgrading, but <?php echo $name->first_name; ?>, that's not all. You get to go everywhere, isn't that cool?
+</p>
